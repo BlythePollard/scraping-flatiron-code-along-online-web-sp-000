85117215333234dbfa19209569c9274980518285
Blythe Pollard <blytheepollard@gmail.com>
2019-10-27 04:47:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -44,6 +44,7 @@ class Scraper
 
 def homepage_genres
   doc = Nokogiri::HTML(open(site))
+  binding.pry
   doc.css("div.gr-list0flinks").each do |genres|
   genres.css("li.gr-list0flinks__item").text
 end
